40053e0e6a5a652ca897f4a5e1b1898a3ad1ed63
Richard Huang <richardhuang.huang@gmail.com>
2022-10-13 10:59:38 -0500

    [refs] Add references page

--- src/main.rs
+++ src/main.rs
@@ -137,14 +137,93 @@ async fn log(Path(repo): Path<String>) -> Html<String> {
 }
 
 async fn refs(Path(repo): Path<String>) -> Html<String> {
-    Html(format!(
-        "{}\n\
-        <h1>[wip] refs</h1>\n\
-        <h1>Repository: {repo}</h1>\n\
-        {}",
-        header(),
-        footer()
-    ))
+    let config = Config::load();
+    let mut result = String::new();
+    result.push_str(header());
+    result.push_str(&format!("<h1>{repo}</h1>"));
+    result.push_str(&format!("<span>git clone git://{repo}.git</span>"));
+    result.push_str(&format!(
+        "<span>
+    <a href=\"/{repo}/log\">Log</a>
+    <a href=\"/{repo}/tree\">Tree</a>
+    <a href=\"/{repo}/refs\">Refs</a>
+            </span>"
+    ));
+    result.push_str("<hr/>");
+
+    let repo = Repository::open(std::path::Path::new(&config.dir).join(repo)).unwrap();
+    result.push_str("<h2>Branches</h2>");
+    result.push_str(
+        "<table>
+        <thead>
+        <tr>
+        <td><b>Name</b></td>
+        <td><b>Last commit date</b></td>
+        <td><b>Author</b></td>
+        </tr>
+        </thead>",
+    );
+    for reference in repo
+        .references()
+        .unwrap()
+        .filter(|r| r.as_ref().unwrap().is_branch())
+    {
+        let r = reference.unwrap();
+        result.push_str("<tr>");
+        result.push_str(&format!("<td>{}</td>", &r.shorthand().unwrap().to_string()));
+        let commit = r.peel_to_commit().unwrap();
+        let naive = NaiveDateTime::from_timestamp(commit.time().seconds(), 0);
+        let datetime: DateTime<Utc> = DateTime::from_utc(naive, Utc);
+        let formatted_datetime = datetime.format("%Y-%m-%d %H:%M");
+        result.push_str(&format!(
+            "<td>{}</td>",
+            formatted_datetime,
+        ));
+        result.push_str(&format!(
+            "<td>{}</td>",
+            &commit.author().to_string()
+        ));
+        result.push_str("</tr>");
+    }
+    result.push_str("</table>");
+
+    result.push_str("<h2>Tags</h2>");
+    result.push_str(
+        "<table>
+        <thead>
+        <tr>
+        <td><b>Name</b></td>
+        <td><b>Last commit date</b></td>
+        <td><b>Author</b></td>
+        </tr>
+        </thead>",
+    );
+    for reference in repo
+        .references()
+        .unwrap()
+        .filter(|r| r.as_ref().unwrap().is_tag())
+    {
+        let r = reference.unwrap();
+        result.push_str("<tr>");
+        result.push_str(&format!("<td>{}</td>", &r.shorthand().unwrap().to_string()));
+        let commit = r.peel_to_commit().unwrap();
+        let naive = NaiveDateTime::from_timestamp(commit.time().seconds(), 0);
+        let datetime: DateTime<Utc> = DateTime::from_utc(naive, Utc);
+        let formatted_datetime = datetime.format("%Y-%m-%d %H:%M");
+        result.push_str(&format!(
+            "<td>{}</td>",
+            formatted_datetime,
+        ));
+        result.push_str(&format!(
+            "<td>{}</td>",
+            &commit.author().to_string()
+        ));
+        result.push_str("</tr>");
+    }
+    result.push_str("</table>");
+
+    result.push_str(footer());
+    Html(result)
 }
 
 async fn tree(Path((repo, path)): Path<(String, String)>) -> Html<String> {
